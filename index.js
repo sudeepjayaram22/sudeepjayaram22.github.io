1a0e61cb12a66785d08ca0c3d7d7bba0fc323bb6
sudeepjayaram22 <sudeepjayaram22@gmail.com>
2023-01-21 01:54:09 +0100

    commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function openWAChat() {
+    let phoneNumberInput = document.getElementById('phoneInputTextBox').value;
+    phoneNumberInput = phoneNumberInput.replace(/\s/g, '').replace("+", "");
+    window.location.assign('https://api.whatsapp.com/send/?phone=%2B' + phoneNumberInput + '&text&type=phone_number&app_absent=0');
+
+
+    setTimeout(() => {
+        document.getElementById("action-button").click();
+    }, 5000);
+
+}
+
+//https://api.whatsapp.com/send/?phone=%2B919901352525&text&type=phone_number&app_absent=0
