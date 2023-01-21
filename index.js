b965fc3bdb3648aab83fb40f4e86987b35931ea4
sudeepjayaram22 <sudeepjayaram22@gmail.com>
2023-01-21 02:21:16 +0100

    replace hyphen

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 function openWAChat() {
     let phoneNumberInput = document.getElementById('phoneInputTextBox').value;
-    phoneNumberInput = phoneNumberInput.replace(/\s/g, '').replace("+", "");
+    phoneNumberInput = phoneNumberInput.replace(/\s/g, '').replace("+", "").replace("-","");
     window.location.assign('https://api.whatsapp.com/send/?phone=%2B' + phoneNumberInput + '&text&type=phone_number&app_absent=0');
 
 
