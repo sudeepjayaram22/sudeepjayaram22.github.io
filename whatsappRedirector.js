288a9586874e96779b5f25495a41475c92c1b498
Sudeep Jayaram <58319818+sudeepjayaram22@users.noreply.github.com>
2023-04-06 14:19:07 +0200

    Add files via upload

---
+++ whatsappRedirector.js
@@ -0,0 +1,12 @@
+
+function openWAChat() {
+    let phoneNumberInput = document.getElementById('phoneInputTextBox').value;
+    phoneNumberInput = phoneNumberInput.replace(/\s/g, '').replace("+", "").replace("-","");
+    window.location.assign('https://api.whatsapp.com/send/?phone=%2B' + phoneNumberInput + '&text&type=phone_number&app_absent=0');
+
+
+    setTimeout(() => {
+        document.getElementById("action-button").click();
+    }, 5000);
+
+}
